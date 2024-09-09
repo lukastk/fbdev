3e175ff347ff596232cf3d32c4644122ebbdce62
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2024-09-09 14:45:53 +0100

    feat(export_tests.py): script now also runs nbdev_clean on test notebooks. Also ran it on the existing tests.

--- export_tests.py
+++ export_tests.py
@@ -1,10 +1,15 @@
 from nbdev.export import nb_export
+from nbdev.clean import clean_nb
+from execnb.nbio import read_nb, write_nb
+from nbdev.process import first_code_ln
 import glob
 import os
 
 # Find all .ipynb files in nbs/tests recursively
 notebook_files = glob.glob('test_nbs/**/*.ipynb', recursive=True)
 
-# Export each notebook file
 for notebook_file in notebook_files:
-    nb_export(notebook_file, 'tests')
+    nb = read_nb(notebook_file)
+    clean_nb(nb) # Clean notebook file
+    write_nb(nb, notebook_file) 
+    nb_export(notebook_file, 'tests') # Export notebook file
