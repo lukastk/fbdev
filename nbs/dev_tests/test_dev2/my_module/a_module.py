dd2a9ca8f1d28696ff2e93885384e6c1357d5b59
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2024-09-30 17:27:13 +0100

    feat: allow for external locations in packet registry

--- nbs/dev_tests/test_dev2/my_module/a_module.py
+++
@@ -1,6 +0,0 @@
-has_been_called = False
-
-def foo():
-    global has_been_called
-    print("Hello world again")
-    has_been_called = True
--- nbs/dev_tests/test_dev2/my_module/my_submodule/my_subsubmodule.py
+++
@@ -1,5 +0,0 @@
-from ..a_module import foo
-import my_module
-
-foo()
-
